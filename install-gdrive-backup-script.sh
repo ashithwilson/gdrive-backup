9c49ff6229b1ece7f34f76a702f976993b43c416
ashithwilson <51533375+ashithwilson@users.noreply.github.com>
2019-10-26 02:05:41 +0530

    Update install-gdrive-backup-script.sh

--- install-gdrive-backup-script.sh
+++ install-gdrive-backup-script.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 install_gdrive_binary()
@@ -6,8 +7,8 @@ if [ -f "/sbin/gdrive" ]; then
 echo "Looks like /sbin/gdrive already exists. Aborting installation!"
 exit 1
 else
-    wget -O /sbin/gdrive https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
-	chmod +x /sbin/gdrive
+    wget -qO /sbin/gdrive https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
+        chmod +x /sbin/gdrive
 fi
 }
 
@@ -18,14 +19,14 @@ dry_run_gdrive()
 
 configure_sync_folder()
 {
-folder_id=`/sbin/gdrive mkdir `hostname`-backups | awk "{print $2}"`
+folder_id=`/sbin/gdrive mkdir $(hostname)-backups | awk '{print $2}'`
 echo "Created Gdrive folder `hostname`-backups with ID $folder_id. The same would be used in backup script - /gdrive-backups/backup.sh"
 }
 
 install_backup_script()
 {
 mkdir /gdrive-backups/
-wget -O /gdrive-backups/backup.sh http://raw.githubusercontent.com/ashithwilson/server-side/master/gdrive-backup.sh
+wget -qO /gdrive-backups/backup.sh http://raw.githubusercontent.com/ashithwilson/server-side/master/gdrive-backup.sh
 sed -i "s|REPLACE_WITH_GDRIVE_FOLDER_ID|$folder_id|g" /gdrive-backups/backup.sh
 chmod +x /gdrive-backups/backup.sh
 }
@@ -34,7 +35,9 @@ print_summary()
 {
 touch /gdrive-backups/mysql_backup_list.txt
 touch /gdrive-backups/backup-list.txt
-echo "
+echo '
+
+============
 ============
 Backup script is installed at /gdrive-backups/backup.sh
 Check gdrive details using command "gdrive help"
@@ -44,17 +47,17 @@ Db - backup list: /gdrive-backups/backup-list.txt
 
 ** Do not forget to set up daily/weekly cron for backups **
 
-"
+'
 }
 
 
-read -p "Are you sure to install gdrive backups?" opt
-if [ $opt -eq "Y" ] || [ $opt -eq "y" ]; then
-{
+read -p "Are you sure to install gdrive backups? [Y/n]: " opt
+if [ "$opt" == "Y" ] || [ "$opt" == "y" ]; then
 install_gdrive_binary
 dry_run_gdrive
 configure_sync_folder
 install_backup_script
 print_summary
-}
-
+else
+echo "Aborting!!!"
+fi
